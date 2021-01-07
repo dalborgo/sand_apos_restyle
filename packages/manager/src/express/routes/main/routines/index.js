2f010610f1f1aa1bc212afee43b974549c49baf4
dalborgo <marco@dalborgo.it>
2021-01-07 18:43:04 +0100

    export

--- packages/manager/src/express/routes/main/routines/index.js
+++ packages/manager/src/express/routes/main/routines/index.js
@@ -1,21 +1,21 @@
 import fs from 'fs'
 import path from 'path'
 import readline from 'readline'
+import get from 'lodash/get'
 
 async function processArchive () {
-  const payments = [], closings = [], keys = {}
-  const fileStream = fs.createReadStream(path.join(__dirname, 'files', 'archivio_short.json'))
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  })
+  const prechecks = [], closings = [], keys = {}
+  const path_ = path.join(__dirname, 'files', 'archivio_short.json')
+  if (!fs.existsSync(path_)) {throw Error(path_ + ' not found!')}
+  const fileStream = fs.createReadStream(path_)
+  const rl = readline.createInterface({ input: fileStream, crlfDelay: Infinity })
   let cont = 0
   for await (const line of rl) {
     const doc = JSON.parse(line)
     switch (doc.type) {
       case 'CLOSING_DAY': {
         // eslint-disable-next-line no-unused-vars
-        const { _id, _meta_id, close_date, date, type, ...rest } = doc
+        const { _id, _meta_id, type, ...rest } = doc
         closings.push(rest)
         keys[doc._meta_id] = cont++
         break
@@ -24,7 +24,7 @@ async function processArchive () {
         const { payments_expanded: paymentsExpanded } = doc
         for (let paymentExpanded of paymentsExpanded) {
           if (paymentExpanded.mode === 'PRECHECK') {
-            payments.push(paymentExpanded)
+            prechecks.push(paymentExpanded)
           }
         }
         break
@@ -33,16 +33,15 @@ async function processArchive () {
         return
     }
   }
-  return { closings, payments, keys }
+  return { closings, prechecks, keys }
 }
 
-async function processMerged (closings, closingKeys) {
+async function processMerged (closings, prechecks, closingKeys) {
   const docs = [], keys = {}
-  const fileStream = fs.createReadStream(path.join(__dirname, 'files', 'astenpos_short.json'))
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  })
+  const path_ = path.join(__dirname, 'files', 'astenpos_short.json')
+  if (!fs.existsSync(path_)) {throw Error(path_ + ' not found!')}
+  const fileStream = fs.createReadStream(path_)
+  const rl = readline.createInterface({ input: fileStream, crlfDelay: Infinity })
   let cont = 0
   for await (const line of rl) {
     const doc = JSON.parse(line)
@@ -51,10 +50,10 @@ async function processMerged (closings, closingKeys) {
       const { _meta_id, close_date, date, type, ...rest } = doc
       docs.push({
         _meta_id,
-        blue: rest,
+        blue: { close_date, date, ...rest },
         close_date,
         date,
-        red: closings[closingKeys[_meta_id]],
+        red: get(closings, closingKeys[_meta_id]),
         type,
       })
     } else {
@@ -62,15 +61,68 @@ async function processMerged (closings, closingKeys) {
     }
     keys[doc._meta_id] = cont++
   }
+  for (let precheck of prechecks) {
+    const { _id, ...rest } = precheck
+    docs.push({ _meta_id: _id, ...rest })
+    keys[precheck._id] = cont++
+  }
   return { docs, keys }
+}
+
+const checkString = (val, key) => val && typeof val === 'string' && key !== '_meta_id'
+const checkObject = val => val && typeof val === 'object'
+
+function findVal (object, keys, token) {
+  Object.keys(object).forEach(function (key) {
+    if (checkString(object[key], key)) {
+      const val = object[key]
+      if (keys[val]) {
+        object[key] = `${val}_${token}`
+      }
+    } else if (object[key] && Array.isArray(object[key])) {
+      let cont = 0
+      for (let arrVal of object[key]) {
+        if (checkObject(object[key])) {
+          findVal(arrVal, keys, token)
+        } else if (checkString(arrVal, key)) {
+          if (keys[arrVal]) {
+            object[key][cont++] = `${arrVal}_${token}`
+          }
+        }
+      }
+    } else if (checkObject(object[key])) {
+      findVal(object[key], keys, token)
+    }
+  })
 }
 
 function addRouters (router) {
   router.get('/routines/migration', async function (req, res) {
-    const { closings, payments, keys: closingKeys } = await processArchive()
-    const { docs: docsAstenpos } = await processMerged(closings, closingKeys)
-    const results = [...docsAstenpos, ...payments]
-    res.send({ ok: true, results })
+    const token = 'prova'
+    const path_ = path.join(__dirname, 'files', 'merged.json')
+    if (fs.existsSync(path_)) {fs.unlinkSync(path_)}
+    const { closings, prechecks, keys: closingKeys } = await processArchive()
+    const { docs: docsAstenpos, keys } = await processMerged(closings, prechecks, closingKeys)
+    
+    //region ricerca chiavi negli oggetti e aggiunge `token`
+    for (let doc of docsAstenpos) {
+      findVal(doc, keys, token)
+    }
+    //endregion
+    const file = fs.createWriteStream(path_, {
+      flags: 'a',
+    })
+    //region modifica `_meta_id` e aggiunge `token`
+    let cont = 0
+    for (let doc of docsAstenpos) {
+      cont++
+      doc['_meta_id'] = `${doc['_meta_id']}_${token}`
+      file.write(JSON.stringify(doc) + (cont < docsAstenpos.length ? '\n' : ''))
+    }
+    file.end()
+    //endregion
+    
+    res.send({ ok: true, results: docsAstenpos })
   })
 }
 
