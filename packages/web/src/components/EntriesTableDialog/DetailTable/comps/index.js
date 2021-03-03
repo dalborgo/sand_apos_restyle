6f4bbb3c7a51b8eb8581ea247e1b24022d5ddbc6
dalborgo <Montebaldo1>
2021-03-03 20:43:16 +0100

    form salvataggio pagamenti

--- packages/web/src/components/EntriesTableDialog/DetailTable/comps/index.js
+++ packages/web/src/components/EntriesTableDialog/DetailTable/comps/index.js
@@ -1,16 +1,37 @@
 import { withStyles } from '@material-ui/core'
-import React from 'react'
+import React, { useMemo } from 'react'
 import Box from '@material-ui/core/Box'
 import { Table } from '@devexpress/dx-react-grid-material-ui'
 import { useTimeFormatter } from 'src/utils/formatters'
 import { messages } from 'src/translations/messages'
 import { useIntl } from 'react-intl'
+import useAuth from 'src/hooks/useAuth'
+import { useQueryClient } from 'react-query'
+import { useLocation, useParams } from 'react-router'
 
 const CellBase = props => {
-  const { column, row, theme } = props
+  const { column, row, theme, tableRow: { rowId } } = props
+  const { selectedCode: { code: owner } } = useAuth()
+  const queryClient = useQueryClient()
+  const { docId } = useParams()
+  const { pathname } = useLocation()
+  const isLast = useMemo(() => {
+    const urlKey = pathname.replace('/app/', '').replace(`/${docId}`, '').replace('-', '_').slice(0, -1)
+    const previousData = queryClient.getQueryData([`${urlKey}/${docId}`, { owner }])
+    return rowId + 1 === previousData?.results?.entries?.length
+  }, [docId, owner, pathname, queryClient, rowId])
   const timeFormatter = useTimeFormatter()
   const intl = useIntl()
-  const cellStyle = { paddingLeft: theme.spacing(2) }
+  const cellStyle =
+    {
+      paddingLeft: theme.spacing(2),
+      ...isLast ?
+        {
+          borderBottomColor: theme.palette.background.paper,
+        }
+        : {},
+    }
+  
   if (column.name === 'date') {
     return (
       <Table.Cell {...props} style={cellStyle}>
